1c390ddd711a501eefdab66c12470c937ff59f37
vdeo02 <deova21@wfu.edu>
2024-09-28 17:41:05 -0500

    Prettier

--- project5392/src/App.js
+++ project5392/src/App.js
@@ -1,24 +1,19 @@
-import './App.css';
-import LoginForm from './Components/LoginForm/LoginForm';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Mainpage from './Components/MainPage/MainPage';
-import Operatorpage from './Components/MainPage/OperatorMainPage';
+import "./App.css";
+import LoginForm from "./Components/LoginForm/LoginForm";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Mainpage from "./Components/MainPage/MainPage";
+import Operatorpage from "./Components/MainPage/OperatorMainPage";
 
 function App() {
   return (
-    <div className='App'>
-   
+    <div className="App">
       <Router>
         <Routes>
-       
-        <Route path="/" element={<LoginForm />} />
-        <Route path="/MainPage" element={<Mainpage />} />
-        <Route path="/OperatorMainPage" element={<Operatorpage />} />
-        <Route path="/SystemBuffer" element={<LoginForm />} />
-
-        
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/MainPage" element={<Mainpage />} />
+          <Route path="/OperatorMainPage" element={<Operatorpage />} />
+          <Route path="/SystemBuffer" element={<LoginForm />} />
         </Routes>
-      
       </Router>
     </div>
   );
